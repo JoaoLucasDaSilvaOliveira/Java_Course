345f02b79fe6fee699f0729d0a68f587e1e78ce3
João Lucas <jlso9866@gmail.com>
2025-05-19 20:19:11 -0300

    Iterando um array

--- 7_COLECOES_DE_DADOS/src/_1_Arrays/App2.java
+++ 7_COLECOES_DE_DADOS/src/_1_Arrays/App2.java
@@ -4,6 +4,19 @@ public class App2 {
     public static void main(String[] args) {
         //iterando um array
         //forma oldschool
-        
+        int [] primeNumbers = {2,5,7,11,13,17,19};
+        for (int i =0; i< primeNumbers.length; i++){
+            System.out.printf("=>%d ", primeNumbers[i]);
+        }
+        System.out.println("\n");
+
+        //enhanced for
+        //oq acontece aqui?
+        // crio uma variável para conter o que tem dentro do array a cada iteração
+        //  :
+        //  passo o array a iterar
+        for (int i: primeNumbers){
+            System.out.printf("=>%d ", i);
+        }
     }
 }
